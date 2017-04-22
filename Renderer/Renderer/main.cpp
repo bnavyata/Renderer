83254f1ddf8fdee4cd3b434388cb5ba3e1f29455
Navyata Bawa <navvubawa@gmail.com>
2017-04-22 11:03:51 -0700

    Added ability to read from element buffer

--- Renderer/Renderer/main.cpp
+++ Renderer/Renderer/main.cpp
@@ -36,11 +36,18 @@ void readShaderSource(char* filename, string &out) {
 GLuint VBO;//vertex buffer object
 GLuint shaderProgram;
 GLuint VAO;
+GLuint EBO;
 
 GLfloat vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f,  0.5f, 0.0f
+	0.5f,  0.5f, 0.0f,  // Top Right
+	0.5f, -0.5f, 0.0f,  // Bottom Right
+	-0.5f, -0.5f, 0.0f,  // Bottom Left
+	-0.5f,  0.5f, 0.0f   // Top Left 
+};
+
+GLuint indices[] = {  // Note that we start from 0!
+	0, 1, 3,   // First Triangle
+	1, 2, 3    // Second Triangle
 };
 
 void Initialize() {	
@@ -59,12 +66,6 @@ void Initialize() {
 
 	const GLchar* vertexShaderSource = tempStr.c_str();
 
-	string tempStr2;
-	readShaderSource("Shaders/basic_fs.glsl", tempStr2);
-
-	const GLchar* fragmentShaderSource = tempStr2.c_str();
-
-
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
@@ -76,6 +77,10 @@ void Initialize() {
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
+
+	readShaderSource("Shaders/basic_fs.glsl", tempStr);
+
+	const GLchar* fragmentShaderSource = tempStr.c_str();	
 
 	/* fragment shader*/
 	GLuint fragmentShader;
@@ -109,10 +114,13 @@ void Initialize() {
 	// MODEL
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 		glEnableVertexAttribArray(0);
@@ -127,7 +135,9 @@ void Draw()
 	glClear(GL_COLOR_BUFFER_BIT);	
 	glUseProgram(shaderProgram);
 	glBindVertexArray(VAO);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 }
 
